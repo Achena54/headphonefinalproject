4373c7da138191927b97e16e93ee1ce96baca145
Achena54 <sarkarachena@gmail.com>
2026-02-08 02:33:25 +0530

    changes in html and css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
- var swiper = new Swiper(".mySwiper", {
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+ const btn = document.getElementById('menu-btn');
+  const menu = document.getElementById('menu');
+
+   btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+
+  new Swiper(".headphoneSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
       },
-    });
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
