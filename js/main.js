956d77387d0023170bc766999b0bfc3a08a8daf2
Achena54 <sarkarachena@gmail.com>
2026-02-09 03:23:07 +0530

    some changes in html

--- js/main.js
+++ js/main.js
@@ -21,3 +21,73 @@
       },
     },
   });
+
+  $(function () {
+
+  $('#mailcheck').hide();
+  $('#pwdcheck').hide();
+
+  let user_no_err = true;
+  let pass_no_err = true;
+
+  $('#mailuser').on('keyup', mail_check)
+
+  function mail_check() {
+    let data = $('#mailuser').val();
+    console.log("mailuser", data);
+    if (data.length < 1) {
+
+      $('#mailcheck').show();
+      $('#mailcheck').html("* required field")
+      user_no_err = false;
+
+    }
+    else if (data.length < 4) {
+      $('#mailcheck').show();
+      $('#mailcheck').html("* min length 4")
+      user_no_err = false;
+    }
+
+    else {
+      $('#mailcheck').hide();
+      user_no_err = true;
+    }
+
+  }
+  $('#pwduser').on('keyup', pass_check)
+
+  function pass_check() {
+    let data = $('#pwduser').val();
+    console.log("pwduser", data);
+    if (data.length < 1) {
+
+      $('#pwdcheck').show();
+      $('#pwdcheck').html("* required field")
+      pass_no_err = false;
+
+    }
+    else if (data.length < 4) {
+      $('#pwdcheck').show();
+      $('#pwdcheck').html("* min length 4")
+      pass_no_err = false;
+    }
+
+    else {
+      $('#pwdcheck').hide();
+      pass_no_err = true;
+    }
+
+  }
+
+  $('#rform').submit(function () {
+    mail_check();
+    pass_check();
+
+    if (user_no_err == true && pass_no_err == true) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  })
+})
